9b34b42c9ac793cc4ef9f1666c1146971d6190cb
Sago Mac <pomprok22@gmail.com>
2025-10-21 18:42:40 +0300

    мультиязык, пункт меню на настройки модуля

---
+++ upload/admin/language/en-gb/extension/module/qmenu.php
@@ -0,0 +1,34 @@
+<?php
+$_['heading_title'] = 'Qmenu';
+
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Success: You have modified Qmenu settings!';
+$_['text_edit'] = 'Edit Qmenu';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+
+$_['entry_status'] = 'Status';
+$_['entry_button_label'] = 'Button label';
+$_['entry_items'] = 'Menu items';
+
+$_['column_label'] = 'Label';
+$_['column_type'] = 'Type';
+$_['column_destination'] = 'Destination';
+$_['column_new_tab'] = 'New tab';
+$_['column_action'] = 'Action';
+
+$_['text_type_link'] = 'Link';
+$_['text_type_route'] = 'Admin route';
+$_['text_no_items'] = 'No items have been added yet.';
+$_['text_default_label'] = 'Qmenu';
+$_['text_empty_menu'] = 'No shortcuts configured.';
+$_['text_manage_qmenu'] = 'Configure Qmenu';
+
+$_['help_link'] = 'Absolute or relative URL (e.g. https://example.com or index.php?route=catalog/product).';
+$_['help_route'] = 'Admin route without token (e.g. catalog/product).';
+$_['help_new_tab'] = 'Check to open the link in a new browser tab.';
+
+$_['button_add_item'] = 'Add item';
+$_['button_remove'] = 'Remove';
+
+$_['error_permission'] = 'Warning: You do not have permission to modify Qmenu!';
---
+++ upload/admin/language/uk-ua/extension/module/qmenu.php
@@ -0,0 +1,34 @@
+<?php
+$_['heading_title'] = 'Qmenu';
+
+$_['text_extension'] = 'Розширення';
+$_['text_success'] = 'Успіх: Налаштування Qmenu збережено!';
+$_['text_edit'] = 'Редагування Qmenu';
+$_['text_enabled'] = 'Увімкнено';
+$_['text_disabled'] = 'Вимкнено';
+
+$_['entry_status'] = 'Статус';
+$_['entry_button_label'] = 'Підпис кнопки';
+$_['entry_items'] = 'Пункти меню';
+
+$_['column_label'] = 'Назва';
+$_['column_type'] = 'Тип';
+$_['column_destination'] = 'Призначення';
+$_['column_new_tab'] = 'Нова вкладка';
+$_['column_action'] = 'Дія';
+
+$_['text_type_link'] = 'Посилання';
+$_['text_type_route'] = 'Адмін-маршрут';
+$_['text_no_items'] = 'Пункти ще не додані.';
+$_['text_default_label'] = 'Qmenu';
+$_['text_empty_menu'] = 'Ярлики ще не налаштовані.';
+$_['text_manage_qmenu'] = 'Налаштувати Qmenu';
+
+$_['help_link'] = 'Абсолютне або відносне посилання (наприклад, https://example.com або index.php?route=catalog/product).';
+$_['help_route'] = 'Маршрут адмінки без токена (наприклад, catalog/product).';
+$_['help_new_tab'] = 'Якщо позначено, посилання відкриється у новій вкладці.';
+
+$_['button_add_item'] = 'Додати пункт';
+$_['button_remove'] = 'Видалити';
+
+$_['error_permission'] = 'Увага: У вас немає прав для зміни Qmenu!';
